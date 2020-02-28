d01180e5b80a72948016c402c4a60fafe7094f3a
Richard Hernandez <r.a.hernandez@outlook.com>
2020-02-28 11:08:13 +0000

    Validation

--- RhzLearnRest.Domains/Models/Dtos/NewCourseDto.cs
+++ RhzLearnRest.Domains/Models/Dtos/NewCourseDto.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace RhzLearnRest.Domains.Models.Dtos
 {
-    public class NewCourseDto
+    public class NewCourseDto : IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from the title.",
+                    new[] { "NewCourseDto" });
+            }
+        }
     }
 }
